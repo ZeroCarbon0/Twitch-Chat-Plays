c9e1b8f3045e0fcbfe0ebb29d1cdd8fbf44470b6
Zerocarbon0 <zerocarboncontact@gmail.com>
2024-08-07 21:30:35 -0400

    Test to see if this works and updates github

--- Twitch_Chat_Plays_Script.py
+++ Twitch_Chat_Plays_Script.py
@@ -1,3 +1,6 @@
 ''' Twitch Chat Plays
 Python code with the goal of letting twitch chat type message and then interact with my computer, stream, or game. 
 KEEP IT SIMPLE STUPID'''
+
+# testing first change and seeing if this commits to github
+
